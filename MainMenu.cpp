1f74e734897ec84a2cef3c449833761b79334b0d
jerica08 <jericamarquez88@gmail.com>
2024-08-19 21:33:48 +0800

    start
    I started with the method called DisplayList.

---
+++ MainMenu.cpp
@@ -0,0 +1,39 @@
+#include <iostream>;
+
+ using namespace std;
+
+int main(){
+      
+      
+
+}
+
+static void DisplayList () {
+
+        int choice;
+        int input;
+
+        cout << "    LIST FILE DETAIL\n";
+        cout << "--------------------------\n";
+        cout << "1. List All Files\n";
+        cout << "2. List of Extension Files\n";
+        cout << "3. List of Name Wise\n";
+        cout << " Enter the Number:\n";
+        cin >> input;
+
+        switch (choice) {
+
+
+            
+        }
+
+
+
+
+
+
+
+
+
+
+}
