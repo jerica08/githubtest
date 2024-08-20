e2e0843e46601e83978a562c6361eec6d13e439a
jerica08 <jericamarquez88@gmail.com>
2024-08-20 08:54:44 +0800

    Main
    Main method update

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,11 +1,26 @@
 #include <iostream>;
 #include <fstream>;
+#include <string>;
 
  using namespace std;
+ 
 
 int main(){
+
       
+       int input;
+
+       cout << "     MAIN MENU    \n"; 
+       cout << "------------------\n"; 
+       cout << "1. To Display List of Files\n";
+       cout << "2. To Create New Directory\n";
+       cout << "3. To Change the Working Directory\n";
+       cout << "Exit\n";
+       cout << "Enter the Number:\n";
+       cin >> input;
       
+
+
 
 }
 
