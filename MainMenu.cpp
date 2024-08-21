e356e08079ecb1d3db8cddcd24131cc59e47083f
jerica08 <jericamarquez88@gmail.com>
2024-08-21 12:08:46 +0800

    Creating the methods
    I first create a blank methods and start the filling with coding the first method which is the mainMenu

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
+#include <string>
 
-                
 
+void mainMenu(){
 
-            
+   cout << "     MENU OPTIONS\n";
+   cout << "----------------------\n";
+   cout << "1. To Display List of Files\n";
+   cout << "2.To Create  New Directory\n";
+   cout << "3. To Change the Working Directory\n";
+   cout << "4.Exit\n";
+   cout << "Enter the Number: \n";     
 
+}
+void listFiles(){
+}            
+void createDirectory(){
+
+} 
+void changeDirectory(){
+
+}
+void listFiles(){
+}            
+void extensionFiles(){        
+}
+void nameWise(){        
+}
