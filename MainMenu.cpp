1f40559fd6a526c87212b934f15bd51d0aede517
jerica08 <jericamarquez88@gmail.com>
2024-08-21 11:50:46 +0800

    Back to 0
    Deleted all and start again

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,46 +1,6 @@
-#include <iostream>;
-#include <fstream>;
-#include <string>;
-
- using namespace std;
- 
-
-int main(){
-
-      
-       int input;
-
-       cout << "     MAIN MENU    \n"; 
-       cout << "------------------\n"; 
-       cout << "1. To Display List of Files\n";
-       cout << "2. To Create New Directory\n";
-       cout << "3. To Change the Working Directory\n";
-       cout << "Exit\n";
-       cout << "Enter the Number:\n";
-       cin >> input;
-      
-
-
-
-}
-
-static void DisplayList () {
-
-        int choice;
-        int input;
-
-        cout << "    LIST FILE DETAIL\n";
-        cout << "--------------------------\n";
-        cout << "1. List All Files\n";
-        cout << "2. List of Extension Files\n";
-        cout << "3. List of Name Wise\n";
-        cout << " Enter the Number:\n";
-        cin >> input;
-
 
                 
 
 
             
 
-}
---
+++ Queue.java
@@ -0,0 +1,19 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class Queue {
+    public static void main(String [] args){
+
+
+
+
+
+
+
+
+    }
+    
+}
+
+
