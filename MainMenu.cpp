7d558192a1f1133c869bfaa187f716473fb87367
jerica08 <jericamarquez88@gmail.com>
2024-08-21 12:25:36 +0800

    Update the methods
    I start to code in the rest of the methods but not yet done.

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -14,11 +14,30 @@ void mainMenu(){
 
 }
 void listFiles(){
+   cout << "     LIST FILE DETAIL     \n";
+   cout << "--------------------------\n";
+   cout << "1. List All Files\n";
+   cout << "2. List of Extension Files\n";
+   cout << " 3. List of Name Wise\n";
+   cout << "Enter th Number: \n";
+
 }            
 void createDirectory(){
+   cout << "Enter the Directory name: \n";
+   cout << "Current directory: \n";
 
 } 
 void changeDirectory(){
+   cout << "     Change Directory     \n";
+   cout << "--------------------------\n";
+   cout << "1. Step by Step Backward\n";
+   cout << "2. Goto Root Directory\n";
+   cout << "3. Forward Directory\n";
+   cout << "Enter the Number: \n\n";
+   cout << "Please enter thyer Directory Name:\n";
+   cout << "Current Directory:\n";
+   
+
 
 }
 void listFiles(){
